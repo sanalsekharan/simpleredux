2995d1a97b0a8dc72fcd62f9980645432aa4cafb
sanalsekharan <sanal_sekharan@live.com>
2017-07-20 23:25:37 +0530

    working redux

--- src/Reducers/reducer.jsx
+++ src/Reducers/reducer.jsx
@@ -4,20 +4,27 @@ const initialState = {
   {itemName: 'Pen', qty: 10,id: 2},
   {itemName: 'Bat', qty: 20,id: 3},
   {itemName: 'Hat', qty: 30,id: 4}],
-  cart:["Bag"]
+  cart:[]
 }
 
 export default function reducer(state = initialState, action) {
 
   switch (action.type) {
     case 'ADD_ITEM_TO_CART':
+      const newState = {items:[],cart:[]}
       state.items.map((items)=>{
+        newState.items.push(items)
         if(items.id === action.payload){
-          state.cart.push(items.itemName)
-          return state;
+          newState.cart.push(items.itemName)
+          state.cart.map((cart)=>{
+            if(cart != items.itemName){
+              newState.cart.push(cart)
+            }
+          })
+          return newState;
         }
       })
-      return state;
+      return newState;
       break;
 
     default:
